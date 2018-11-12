64859db46a045e4eb3bd31611150ac5325e803b2
Mayowa Adegeye <adegeye.mayowa@careem.com>
2018-11-12 22:32:45 +0100

    Intial project files commit

---
+++ app/src/main/java/com/android/mayojava/dailyfootball/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.mayojava.dailyfootball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
