c86a11b5e4086227fac32c746d9fc4eb5f9a0e1e
Mayowa Adegeye <adegeye.mayowa@careem.com>
2018-11-24 14:21:21 +0100

    Included league logos

--- data/src/main/java/com/android/mayojava/dailyfootball/data/services/FootballDataApi.kt
+++ data/src/main/java/com/android/mayojava/dailyfootball/data/services/FootballDataApi.kt
@@ -1,4 +1,20 @@
 package com.android.mayojava.dailyfootball.data.services
 
-class FootballDataApi {
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class FootballDataApi(val token: String) {
+}
+
+private class HeaderInterceptor(private val api: FootballDataApi): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("X-Auth-Token", api.token)
+            .build()
+
+        val urlBuilder = request.url().newBuilder()
+        urlBuilder.addQueryParameter("plan", "TIER_ONE")
+        return chain.proceed(request)
+    }
+
 }
