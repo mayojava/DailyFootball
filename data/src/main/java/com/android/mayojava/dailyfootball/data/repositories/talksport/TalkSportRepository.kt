0aa08e54a2e9b97ad1b4973ae77571f1630e6966
Mayowa Adegeye <mayox4ever2006@gmail.com>
2018-12-20 21:57:48 +0100

    Completed TalkSport Repository implementation

--- data/src/main/java/com/android/mayojava/dailyfootball/data/repositories/talksport/TalkSportRepository.kt
+++ data/src/main/java/com/android/mayojava/dailyfootball/data/repositories/talksport/TalkSportRepository.kt
@@ -1,4 +1,33 @@
 package com.android.mayojava.dailyfootball.data.repositories.talksport
 
-class TalkSportRepository {
+import androidx.paging.DataSource
+import arrow.core.getOrDefault
+import com.android.mayojava.dailyfootball.base.util.AppCoroutineDispatchers
+import com.android.mayojava.dailyfootball.base.util.Logger
+import com.android.mayojava.dailyfootball.data.entities.TalkSportEntity
+import com.android.mayojava.dailyfootball.data.mappers.DiffUtil
+import io.reactivex.Flowable
+import javax.inject.Inject
+
+class TalkSportRepository @Inject constructor(
+    private val localDataSource: TalkSportLocalDataSource,
+    private val remoteDataSource: TalkSportRemoteDataSource,
+    private val dispatchers: AppCoroutineDispatchers,
+    private val logger: Logger) {
+
+    fun observeNews(): Flowable<List<TalkSportEntity>> = localDataSource.observeLatestNews()
+
+    fun observeNewsForPaging(): DataSource.Factory<Int, TalkSportEntity> = localDataSource.observeNewsForPagination()
+
+    suspend fun getNewsById(id: Long): TalkSportEntity = localDataSource.getNewsById(id)
+
+    suspend fun updateTalkSportNews() {
+        val response = remoteDataSource.talkSportNews()
+        if (response.isSuccess()) {
+            val diff = DiffUtil.diff(localDataSource.newsList(), response.getOrDefault { listOf() })
+            localDataSource.inserNews(diff)
+        } else {
+            response.fold({ error -> logger.e(error)}, {})
+        }
+    }
 }
