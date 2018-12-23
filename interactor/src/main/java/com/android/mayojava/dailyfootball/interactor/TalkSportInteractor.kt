d8aff2a5a872821d3fd4b09de831fb09f112e010
Mayowa Adegeye <mayox4ever2006@gmail.com>
2018-12-23 08:47:12 +0100

    only refresh talk sport if outdated

--- interactor/src/main/java/com/android/mayojava/dailyfootball/interactor/TalkSportInteractor.kt
+++ interactor/src/main/java/com/android/mayojava/dailyfootball/interactor/TalkSportInteractor.kt
@@ -1,18 +1,25 @@
 package com.android.mayojava.dailyfootball.interactor
 
 import androidx.paging.DataSource
+import com.android.mayojava.dailyfootball.data.NewsSource
+import com.android.mayojava.dailyfootball.data.daos.LastRefreshTimeDao
 import com.android.mayojava.dailyfootball.data.entities.TalkSportEntity
 import com.android.mayojava.dailyfootball.data.repositories.talksport.TalkSportRepository
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.reactive.openSubscription
 import javax.inject.Inject
 
-class TalkSportInteractor @Inject constructor(private val talkSportRepository: TalkSportRepository):
-    PagingInteractor<TalkSportEntity>, RunnableInteractor<List<TalkSportEntity>>() {
+class TalkSportInteractor @Inject constructor(private val talkSportRepository: TalkSportRepository,
+                                              private val lastRefreshTimeDao: LastRefreshTimeDao):
+    PagingInteractor<TalkSportEntity>, RunnableInteractor<List<TalkSportEntity>>(), NewsRefresher {
 
     override fun dataSourceFactory(): DataSource.Factory<Int, TalkSportEntity> = talkSportRepository.observeNewsForPaging()
 
-    override suspend fun execute() = talkSportRepository.updateTalkSportNews()
+    override suspend fun execute() {
+        refreshIfOutdated(NewsSource.talkSport, lastRefreshTimeDao) {
+            talkSportRepository.updateTalkSportNews()
+        }
+    }
 
     override fun observe(): ReceiveChannel<List<TalkSportEntity>> = talkSportRepository.observeNews().openSubscription()
 
